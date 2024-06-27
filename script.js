f9d11c8bd53a509f2d303278104d504989e745ad
werbnah <andrewchau1993@gmail.com>
2024-06-27 09:17:16 -0700

    noticed i was missing 11 so i just added it in

--- script.js
+++ script.js
@@ -229,7 +229,31 @@ const calculateGrade = (grade) => {
 }
 
 console.log('Exercise 10 Result:', calculateGrade(85));
+/*
+Exercise 11: createUsername()
 
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre12'.
+
+Complete the exercise in the space below:
+*/
+const createUsername = (firstName, lastName) => {
+  const firstPart = firstName.slice(0,3);
+  const lastPart = lastName.slice(0,3);
+  const totalLength = firstName.length + lastName.length;
+  const username = `${firstPart}${lastPart}${totalLength}`;
+  return username;
+}
+
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
 /*
 Exercise 12: numArgs()
 
